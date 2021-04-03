503649145ebb67757ddbe141324b947bda6cd6c6
AriChou <46300506+Ari-Chou@users.noreply.github.com>
2021-04-03 13:35:53 +0800

    Initial Commit

---
+++ AppleStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleStore
+//
+//  Created by AriChou on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
